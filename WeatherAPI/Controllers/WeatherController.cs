9fdca6236d7e6417446b802d807781301d006292
Jordan Roberts <jordanroberts@talk21.com>
2019-09-12 21:46:34 +0100

    Api call successful but only returns the city name correctly, temp and summary are currently null so need to fix

--- WeatherAPI/Controllers/WeatherController.cs
+++ WeatherAPI/Controllers/WeatherController.cs
@@ -1,20 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Newtonsoft.Json;
 
 namespace WeatherAPI.Controllers
 {
     [Route("api/[controller]")]
     public class WeatherController : Controller
     {
-        [HttpGet("[action]/{city}")] 
+        [HttpGet("[action]/{city}")]
         public async Task<IActionResult> City(string city)
         {
-            return Ok(new { Temp = "12", Summary = "Barmy", City = city });
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("http://api.openweathermap.org");
+                    var response = await client.GetAsync($"/data/2.5/weather?q={city}&appid=APIKEY&units=metric");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject<OpenWeatherResponse>(stringResult);
+                    return Ok(new
+                    {
+                        Temp = rawWeather.Temp,
+                        City = rawWeather.Name,
+                        Summary = rawWeather.Description
+                    });
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                }
+            }
+
         }
+
     }
-    
+
+    public class OpenWeatherResponse
+    {
+        public string Name { get; set; }
+        public string Temp { get; set; }
+        public string Description { get; set; }
+    }
+
 }
