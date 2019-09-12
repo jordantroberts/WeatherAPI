f263ebfd4d5c2f3670cdfab810ab090ed4b6fcc0
Jordan Roberts <jordanroberts@talk21.com>
2019-09-12 21:12:38 +0100

    Added basic weather controller

---
+++ WeatherAPI/Controllers/WeatherController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WeatherAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherController : Controller
+    {
+        [HttpGet("[action]/{city}")] 
+        public IActionResult City(string city)
+        {
+            return Ok(new { Temp = "12", Summary = "Barmy", City = city });
+        }
+    }
+    
+}
