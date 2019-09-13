74d58f29f5d9fa4036bb053dfb1e3604cd1dc289
Jordan Roberts <jordanroberts@talk21.com>
2019-09-13 12:57:36 +0100

    Added form so now user can type in their city and the weather API will update based on that

--- WeatherAPI/ClientApp/src/components/Home.js
+++ WeatherAPI/ClientApp/src/components/Home.js
@@ -3,9 +3,12 @@ import React, { Component } from "react";
 export class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { temp: "", summary: "", city: "" };
+    this.state = { temp: "", summary: "", city: "", location: "London" };
+  }
 
-    fetch("api/weather/city/london/")
+    getData(e) {
+        e.preventDefault()
+    fetch("api/weather/city/" + (this.state.location))
       .then(response => response.json())
       .then(data =>
         this.setState({
@@ -16,11 +19,23 @@ export class Home extends Component {
       );
   }
 
+   
+
   render() {
     return (
       <div>
         <center>
           <h1>Weather</h1>
+          <p>Please enter your city:</p>
+          <form onSubmit={(e) => this.getData(e)}>
+            <input
+              type="text"
+              placeholder="Type city here..."
+              value={this.state.location}
+              onChange={e => this.setState({ location: e.target.value })}
+            />
+            <button type="submit">Submit</button>
+          </form>
           <h4>City</h4>
           <p>{this.state.city}</p>
           <h4>Temperature</h4>
